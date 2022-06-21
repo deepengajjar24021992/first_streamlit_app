8141d03aa754f55ea7aa1c43d15f6c1d64858bff
Deepen <107909813+deepengajjar24021992@users.noreply.github.com>
2022-06-21 13:38:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("I'm Deepen Gajjar")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
