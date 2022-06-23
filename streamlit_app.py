715e04aa609b799e2ce51249c186438e9202f519
Deepen <107909813+deepengajjar24021992@users.noreply.github.com>
2022-06-23 13:19:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+impoer requests
 
 streamlit.title("I'm Deepen Gajjar")
 
@@ -19,6 +20,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
