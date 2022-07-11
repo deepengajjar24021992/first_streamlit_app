e22d55fd6f52d57978fe33409e8dc4b191838fe5
Deepen <107909813+deepengajjar24021992@users.noreply.github.com>
2022-07-11 14:18:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
+
+streamlit.text("What fruit would you like to add?")
+streamlit.text_input()
